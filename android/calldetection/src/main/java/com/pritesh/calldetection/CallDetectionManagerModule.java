5e5f5cfe6e8bffd1d9bb6828e35f7b0b3b749020
Pritesh Nandgaonkar <pncooldude91@gmail.com>
2017-06-22 15:54:29 +0530

    Moved files

--- android/calldetection/src/main/java/com/pritesh/calldetection/CallDetectionManagerModule.java
+++ android/calldetection/src/main/java/com/pritesh/calldetection/CallDetectionManagerModule.java
@@ -34,7 +34,7 @@ public class CallDetectionManagerModule
 
     @Override
     public String getName() {
-        return "CallDetectionManager";
+        return "CallDetectionManagerAndroid";
     }
 
     @ReactMethod
--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 /**
  * @providesModule react-native-call-detection
  */
-import { NativeModules, NativeEventEmitter } from 'react-native'
-const NativeCallDetector = NativeModules.CallDetectionManager
+import { NativeModules, NativeEventEmitter, Platform } from 'react-native'
+const BatchedBridge = require('BatchedBridge'
+BatchedBridge.registerCallableModule('CallStateUpdateActionModule', CallDetectorManager)
 
-NativeCallDetector.startListener();
+const NativeCallDetector = NativeModules.CallDetectionManager
+const NativeCallDetectorAndroid = NativeModules.CallDetectionManagerAndroid
+
+NativeCallDetector && NativeCallDetector.startListener();
+NativeCallDetectorAndroid && NativeCallDetectorAndroid.startListener();
 
 class CallDetectorManager {
 
     subscription;
-
+    callback
     constructor(callback) {
-    	this.subscription = new NativeEventEmitter(NativeCallDetector)
-        this.subscription.addListener('PhoneCallStateUpdate', callback);
+        this.callback = callback
+        if (Platform.OS === 'ios') {
+            this.subscription = new NativeEventEmitter(NativeCallDetector)
+            this.subscription.addListener('PhoneCallStateUpdate', callback);
+        }
     }
 
     dispose() {
-    	NativeCallDetector.stopListener()
+    	NativeCallDetector && NativeCallDetector.stopListener()
+    	NativeCallDetectorAndroid && NativeCallDetectorAndroid.stopListener()
         if(this.subscription) this.subscription.remove();
     }
+
+      callStateUpdated(state) {
+        console.log('State')
+        console.log(state)
+        callback(state)
+      }
 }
 
 export default module.exports = CallDetectorManager;
