2a6f7ffffb48ab16da72a55dd33f77fbcbc22bae
Pritesh Nandgaonkar <pncooldude91@gmail.com>
2017-08-17 15:37:56 +0530

    Updated example

--- CallDetectionExample/src/HomeComponent.js
+++ CallDetectionExample/src/HomeComponent.js
@@ -9,6 +9,7 @@ import {
   View,
   Button,
   Linking,
+  ListView
 } from 'react-native';
 
 var callDetector = undefined
@@ -16,9 +17,20 @@ var callDetector = undefined
 import CallDetectorManager from 'react-native-call-detection'
 
 export default class HomeComponent extends Component {
+  constructor(props) {
+    super(props)
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    this.state = {callStates : [], ds: ds} //call states
+    this.startListenerTapped = this.startListenerTapped.bind(this);
+  }
+
   startListenerTapped() {
     callDetector = new CallDetectorManager((event)=>{
-      console.log(event)
+      var updatedCallStates = this.state.callStates
+      updatedCallStates.push(event)
+      var previousDS = this.state.ds
+      this.setState({ callStates:  updatedCallStates, ds: previousDS.cloneWithRows(updatedCallStates)});
     })
   }
 
@@ -34,35 +46,37 @@ export default class HomeComponent extends Component {
   }
 
   render() {
+    console.log(this.state.callStates)
     return (
       <View style={styles.container}>
         <Button
           onPress={this.startListenerTapped}
           title="Start Listener"
           color="#841584"
+          style = {styles.bottomMargin}
         />
 
         <Button
           onPress={this.callFriendTapped}
           title="Call your friend"
           color="#341584"
+          style = {styles.bottomMargin}
         />
-
         <Button
           onPress={this.stopListenerTapped}
           title="Stop Listener"
           color="#841584"
+          style = {styles.bottomMargin}
         />
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+
+        <Text style = {styles.text}>
+          Call State Logs
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
+        <ListView
+          dataSource={this.state.ds}
+          renderRow={(rowData) => <Text style = {styles.callLogs}>{rowData}</Text>}
+        />
       </View>
     );
   }
@@ -70,19 +84,25 @@ export default class HomeComponent extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 40,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  bottomMargin: {
+    marginBottom: 10
+  },
+  text: {
+    marginTop: 30,
+    textAlign:'center',
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    color: '#341584'
   },
-  instructions: {
-    textAlign: 'center',
+  callLogs: {
+    textAlign:'center',
+    fontSize: 15,
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
