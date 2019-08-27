f17095186506b8a5473063d6fec1adc119face62
Alayn Gortazar <alayn@barnetik.com>
2019-08-27 16:46:57 +0200

    Fixed linking/build error. Removed constructor dependency on android.app.Application

--- android/src/main/java/com/pritesh/calldetection/CallDetectionManager.java
+++ android/src/main/java/com/pritesh/calldetection/CallDetectionManager.java
@@ -5,7 +5,6 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import android.app.Application;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -14,19 +13,11 @@ import java.util.Arrays;
 
 public class CallDetectionManager implements ReactPackage {
 
-    private Application applicationInstance;
-
-    public CallDetectionManager(Application applicationInstance) {
-        super();
-        this.applicationInstance = applicationInstance;
-    }
-
     @Override
     public List<NativeModule> createNativeModules(
             ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
         CallDetectionManagerModule callDetectionModule = new CallDetectionManagerModule(reactContext);
-        applicationInstance.registerActivityLifecycleCallbacks(callDetectionModule);
         modules.add(callDetectionModule);
 
         return modules;
